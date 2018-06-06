69dbf670dc7a5f67665f56ee0d8522d58d6d8e07
Ganvir, Amit <amit.ganvir@synapse.com>
2018-06-06 17:36:00 +0530

    Update kafka script

---
+++ kafka-script/server.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+echo "Running server.sh"
+
+region=$1
+adminUsername=$2
+adminPassword=$3
+services=$4
+stackName=$5
+
+apt-get -y update
+apt-get -y install jq
+
+if [ -z "$6" ]
+then
+  echo "This node is part of the autoscaling group that contains the rally point."
+  rallyPrivateDNS=`getrallyPrivateDNS`
+else
+  rallyAutoScalingGroup=$6
+  echo "This node is not the rally point and not part of the autoscaling group that contains the rally point."
+  echo rallyAutoScalingGroup \'$rallyAutoScalingGroup\'
+  rallyPrivateDNS=`getrallyPrivateDNS ${rallyAutoScalingGroup}`
+fi
+
+## Getting Instance ID
+instanceID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.instanceId' \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+nodePrivateDNS=`curl http://169.254.169.254/latest/meta-data/hostname`
+
+## Infor
+echo "Using the settings:"
+echo adminUsername \'$adminUsername\'
+echo adminPassword \'$adminPassword\'
+echo services \'$services\'
+echo stackName \'$stackName\'
+echo rallyPrivateDNS \'$rallyPrivateDNS\'
+echo region \'$region\'
+echo instanceID \'$instanceID\'
+echo nodePrivateDNS \'$nodePrivateDNS\'
+
+if [ -z "$6" ]
+then
+    aws ec2 create-tags \
+    --region ${region} \
+    --resources ${instanceID} \
+    --tags Key=Name,Value=${stackName}-Server Key=Role,Value=${services} Key=StackService,Value=${stackName}-${services}
+else
+    aws ec2 create-tags \
+    --region ${region} \
+    --resources ${instanceID} \
+    --tags Key=Name,Value=${stackName}-Server-Rally Key=Role,Value=${services} Key=StackService,Value=${stackName}-${services}
+fi
